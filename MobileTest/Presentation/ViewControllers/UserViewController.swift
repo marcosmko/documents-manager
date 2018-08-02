cc3c56080648d43f922560df8248d8485735d93e
Marcos Kobuchi <marcos_kobuchi@hotmail.com>
2018-08-01 21:57:35 -0300

    [MOBILE-1] Adds User Basic Controller.

---
+++ MobileTest/Presentation/ViewControllers/UserViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UserViewController.swift
+//  MobileTest
+//
+//  Created by Marcos Kobuchi on 01/08/18.
+//  Copyright © 2018 Marcos Kobuchi. All rights reserved.
+//
+
+import UIKit
+
+public class UserViewController: UIViewController {
+
+    @IBOutlet public weak var pictureImageView: UIImageView!
+    @IBOutlet public weak var emailLabel: UILabel!
+    @IBOutlet public weak var phoneLabel: UILabel!
+    @IBOutlet public weak var addressLabel: UILabel!
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
